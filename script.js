fcf39d088dddf44eb4c3a933824fb9d4d25261d5
Darshan Badiger <140099027+dbadiger@users.noreply.github.com>
2025-01-03 02:07:37 +0000

    Navbar completed

---
+++ script.js
@@ -0,0 +1,9 @@
+const sideMenu = document.querySelector('#sidemenu') 
+
+function OpenMenu (){
+    sideMenu.style. transform='translateX(-16rem)'
+}
+
+function CloseMenu (){
+    sideMenu.style. transform='translateX(16rem)'
+}
