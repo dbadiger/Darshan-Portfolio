bec2f9ea69091f4e118a21170bf958ac784440f4
Darshan Badiger <140099027+dbadiger@users.noreply.github.com>
2025-01-11 17:28:00 +0000

    completed

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const sideMenu = document.querySelector('#sidemenu') 
+const navBar = document.querySelector("nav")
+const navlink = document.querySelector("nav ul")
 
 function OpenMenu (){
     sideMenu.style. transform='translateX(-16rem)'
@@ -6,4 +8,39 @@ function OpenMenu (){
 
 function CloseMenu (){
     sideMenu.style. transform='translateX(16rem)'
+}
+window.addEventListener('scroll', ()=>{
+    if(scrollY>50){
+        navBar.classList.add('bg-white', 'bg-opacity-50', 'background-blur-lg','shadow-sm','dark:bg-darkTheme', 'dark:shadow-white/20')
+        navlink.classList.remove('bg-white', 'shadow-sm', 'bg-opacity-50', 'dark:border', "dark:border-white/50", 'dark:bg-transparent');
+    }
+    else{
+        navBar.classList.remove('bg-white', 'bg-opacity-50', 'background-blur-lg','shadow-sm','dark:bg-darkTheme', 'dark:shadow-white/20')
+        navlink.classList.add('bg-white', 'shadow-sm', 'bg-opacity-50','dark:border',"dark:border-white/50", 'dark:bg-transparent');
+    }
+})
+
+// -----------------light, dark mode-----------
+
+
+// document.documentElement.classList.toggle(
+//     'dark',
+//     localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+//   )
+
+if(localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)){
+    document.documentElement.classList.add('dark')
+}else{
+    document.documentElement.classList.remove('dark')
+}
+
+
+function toggleTheme(){
+    document.documentElement.classList.toggle('dark')
+    if(document.documentElement.classList.contains('dark')){
+        localStorage.theme='dark'
+    }
+    else{
+        localStorage.theme="light"
+    }
 }
---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+tailwind.config = {
+    theme:{
+        extend:{
+
+            gridTemplateColumns:{
+                'auto':'repeat(auto-fit, minmax(200px, 1fr))'
+            },
+            fontFamily:{
+                Outfit:["Outfit", "sans-serif"],
+                Ovo:["Ovo", "sans-serif"]
+            },
+            animation:{
+                spin_slow:'spin 7s linear infinite'
+            },
+            colors:{
+                lightHover:"#dfedff",
+                darktHover:"#2a004a",
+                darkTheme:"#060515"
+            },
+            boxShadow:{
+                'black':"4px 4px 0 #000",
+                'white':"4px 4px 0 #fff",
+                'lightBlue':"0 4px 55px #dfedff"
+            }
+        },
+      
+    },
+    darkMode:'selector'
+    
+}
